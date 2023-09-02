d5fe5ac463b52440fb1d97a1638ffb7c342730e9
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-02 12:46:33 +0200

    Add calculator with basic plus and minus operations. Updated notes.

--- src/01-base/app.js
+++ src/01-base/app.js
@@ -1,65 +1,60 @@
-let num = 32;               // number
-let str = "hello string";   // string
-let first_name = "Roman";   // string
-const isPersistent = true;  // boolean constant
+const firstValueElement = document.getElementById('firstValue');
+const secondValueElement = document.getElementById('secondValue');
+const plusBtn = document.getElementById('plus');
+const minusBtn = document.getElementById('minus');
+const submitBtn = document.getElementById('submit');
+const resultElement = document.getElementById('result');
+
+let action = '+';
 
 /*
-console.log('Before changes:', num, str, isPersistent);
+// get values from the inputs
+// var num1 = firstValueElement.value;
+// var num2 = secondValueElement.value;
 
-num = 43;
-str = 'changed';
-//isPersistent = false;       // error - impossible to change const
+// var sum = num1 + num2;                   // it's just concatenation of strings
+// console.log(typeof sum);                 // concatenated string
 
-console.log('After changes:', num, str, isPersistent);
+//var sum = Number(num1) + Number(num2);    // we mapped string to number type and only after that we can sum two numbers
+//console.log(typeof sum);                  // type = number
 */
 
-/*
-// What can we do with variables?
-let $ = 'test';
-let $num = 44;
-let num$ = 22;
-let _ = 49;
-let _num = 23;
-let num_ = 32;
-let first_name = 'Roman';   // Snake Case style
-let myNum = 12;             // Camel case style
-let LastName = 'Pro';       // Pascal case style
-*/
+plusBtn.onclick = function() {
+    action = '+';
+}
 
-/*
-// Restrictions with variables
-let 23num = 23;           // variable name can't start with a number
-let my-num = 12;          // can't use '-' in a variable name
-let false = 'text';       // can't use reserved words as a variable name
-*/
+minusBtn.onclick = function() {
+    action = '-';
+}
 
-/*
-// Operations with variables and printing
-console.log('Addition result:', num + 10);
-console.log('Subtraction result:', num - 10);
-console.log('Multiplication result:', num * 10);
-console.log('Division result:', num / 10);      // Result = 4.3 float type
-console.log('Original num:', num);              // num will be immutable if we printing the value
-*/
+function printResult(result) {
+    /*
+    // full format
+    if (result < 0) {
+        resultElement.style.color = 'red';
+    }
+    else {
+        resultElement.style.color = 'green';
+    }
+    */
+    // ternary operator
+    resultElement.style.color = result < 0 ? 'red' : 'green';
 
-/*
-// Manipulations with numbers
-let num2 = num + 10;
-console.log(num, num2);
+    resultElement.textContent = result;
+}
 
-num = num2 - num;       // changing num value
-console.log(num);
+function calculateNumbersWithAction(input1, input2, actionSymbol) {
+    const num1 = Number(input1.value);
+    const num2 = Number(input2.value);
 
-num2 = num2 + 1;       // changing num2 value
-console.log(num2);
+    if (actionSymbol == '+') {
+        return num1 + num2;
+    } else if (action == '-') {
+        return num1 - num2;
+    }
+}
 
-let num3 = (num + num2) * 2 - 1; // by default all the Math priorities will be applied
-console.log(num3);
-*/
-
-/*
-// Manipulations with strings
-const fullName = first_name + " Pro";   // concatenation
-console.log(fullName);
-*/
-
+submitBtn.onclick = function() {
+    const result = calculateNumbersWithAction(firstValueElement, secondValueElement, action);
+    printResult(result);
+};
