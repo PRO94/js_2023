40a563107f550ab38ac9313b5d1e4ff99eb18696
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-02 15:48:23 +0200

    Updated operators theory to convert string to number

--- src/01-base/02-operators.js
+++ src/01-base/02-operators.js
@@ -19,3 +19,11 @@ num1 = num1 ** 3; // num1 * num1 * num1
 
 // Additional operators for if
 // > < >= <= == !=
+
+
+// Parse string to number:
+const numAsStr = '23';
+let num = 10;
+// console.log(typeof (numAsStr + num))  // result is string type and equals '2310'
+// console.log(Number(numAsStr) + num)     // result is 33 as it was parsed to number
+// console.log(parseInt(numAsStr) + num)   // result is 33 as it was parsed to number. Second way for parsing.
