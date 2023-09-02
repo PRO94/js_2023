41b533b67154e5fa27c9d2ff58c6a2f5ea3e4d9c
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-02 11:26:17 +0200

    Added basic manipulations with variables

--- src/01-base/app.js
+++ src/01-base/app.js
@@ -1,16 +1,17 @@
 let num = 32;               // number
 let str = "hello string";   // string
-const isPersistent = true;  // constant
+let first_name = "Roman";   // string
+const isPersistent = true;  // boolean constant
 
-console.log("\nBefore changes:");
-console.log(num, str, isPersistent);
+/*
+console.log('Before changes:', num, str, isPersistent);
 
 num = 43;
 str = 'changed';
 //isPersistent = false;       // error - impossible to change const
 
-console.log("\nAfter changes:");
-console.log(num, str, isPersistent, '\n');
+console.log('After changes:', num, str, isPersistent);
+*/
 
 /*
 // What can we do with variables?
@@ -30,5 +31,35 @@ let LastName = 'Pro';       // Pascal case style
 let 23num = 23;           // variable name can't start with a number
 let my-num = 12;          // can't use '-' in a variable name
 let false = 'text';       // can't use reserved words as a variable name
+*/
+
+/*
+// Operations with variables and printing
+console.log('Addition result:', num + 10);
+console.log('Subtraction result:', num - 10);
+console.log('Multiplication result:', num * 10);
+console.log('Division result:', num / 10);      // Result = 4.3 float type
+console.log('Original num:', num);              // num will be immutable if we printing the value
+*/
+
+/*
+// Manipulations with numbers
+let num2 = num + 10;
+console.log(num, num2);
+
+num = num2 - num;       // changing num value
+console.log(num);
+
+num2 = num2 + 1;       // changing num2 value
+console.log(num2);
+
+let num3 = (num + num2) * 2 - 1; // by default all the Math priorities will be applied
+console.log(num3);
+*/
+
+/*
+// Manipulations with strings
+const fullName = first_name + " Pro";   // concatenation
+console.log(fullName);
 */
 
