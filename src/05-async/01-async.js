7aebc686a30b4543496e4790407652cd62c1846f
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-05 14:21:18 +0200

    Add async/await usage example

--- src/05-async/01-async.js
+++ src/05-async/01-async.js
@@ -32,15 +32,35 @@ const delay = (time = 1000) => {
     return promise;
 };
 
-delay(2500)
-    .then((data) => {   // Executed only when resulve()
-        console.log('timeout result', data );
-        return data.map((x) => x ** 2);
-    })
-    .then(data => {     // Executed only when resulve()
-        console.log('after modification', data);
-    })
-    .catch(error => {   // Executed only when reject()
-        console.log(error);
-    })
-    .finally(() => console.log('Executed in any way'))
+// delay(2500)
+//     .then((data) => {   // Executed only when resulve()
+//         console.log('timeout result', data );
+//         return data.map((x) => x ** 2);
+//     })
+//     .then(data => {     // Executed only when resulve()
+//         console.log('after modification', data);
+//     })
+//     .catch(error => {   // Executed only when reject()
+//         console.log(error);
+//     })
+//     .finally(() => console.log('Executed in any way'));
+
+// Example 1
+const getData = () => new Promise((resolve) => resolve([1,2,3]))
+// getData()
+//     .then(array => console.log(array))
+
+// Example 2 - async/await
+async function asyncExample() {
+    try {
+        await delay(3000);
+        const data = await getData();
+        console.log(data);
+    } catch(error) {
+        console.log('Error', error)
+    } finally {
+        console.log('Finally')
+    }
+}
+
+// asyncExample();
