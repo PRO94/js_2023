5e55d218aa9327458fcfb29fc3423e9236d437ec
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-09-05 14:10:02 +0200

    Add async, Promise theory.

---
+++ src/05-async/01-async.js
@@ -0,0 +1,46 @@
+// ========== Async = Event Loop
+
+// const timeout = setTimeout(() => {
+//     console.log('after 2 seconds')
+// }, 2000);
+
+// clearTimeout(timeout);
+
+// setTimeout(() => {
+//     console.log('after 3 seconds')
+// }, 3000);
+
+// let count = 0;
+// setInterval(() => {
+//     console.log('Tick', count++)
+// }, 1000)
+
+// function delay(callback, time = 1000) {
+//     setTimeout(callback, time);
+// }
+// delay(() => console.log('timeout'), 1500)
+
+
+/// Promise
+const delay = (time = 1000) => {
+    const promise = new Promise((resolve, reject) => { 
+        setTimeout(() => {
+            resolve([1, 2, 3]);
+            //reject('Error in delay')
+        }, time)
+    });
+    return promise;
+};
+
+delay(2500)
+    .then((data) => {   // Executed only when resulve()
+        console.log('timeout result', data );
+        return data.map((x) => x ** 2);
+    })
+    .then(data => {     // Executed only when resulve()
+        console.log('after modification', data);
+    })
+    .catch(error => {   // Executed only when reject()
+        console.log(error);
+    })
+    .finally(() => console.log('Executed in any way'))
